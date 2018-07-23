e3b4f894379005c26318a370d168d76af5bcf078
haveza <haveza@gmail.com>
2018-07-23 14:41:05 +0300

    test

--- IdeaProjects/jdbc/src/main/java/main.java
+++ IdeaProjects/jdbc/src/main/java/main.java
@@ -4,6 +4,7 @@ public class main {
 
     public static void main(String[] args) throws SQLException {
         Connection con = null;
+
         if(null == (con = JDBC.InitConn("db"))){ return;}
 
         // Create Query
